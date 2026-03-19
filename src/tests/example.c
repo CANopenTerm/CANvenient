c4b753eeed2efac8d99b2db2b34a58342bf108a9
Michael Fitzmayer <mail@michael-fitzmayer.de>
2026-03-19 15:07:15 +0100

    Simplify example for the time being.

--- src/tests/example.c
+++ src/tests/example.c
@@ -44,40 +44,6 @@ int main()
         }
     }
 
-    struct can_frame out = {0};
-    struct can_frame in = {0};
-    u64 timestamp = 0;
-
-    out.can_id = 0x123;
-    out.can_dlc = 2;
-    out.data[0] = 0xAB;
-    out.data[1] = 0xCD;
-
-    puts("Sending CAN frames from interface 0 and receiving on interface 1...");
-    for (int i = 0; i < 10; i++)
-    {
-        if (can_send(0, &out) < 0)
-        {
-            char error_reason[256] = {0};
-            can_get_error_reason(error_reason, sizeof(error_reason));
-            printf("%s\n", error_reason);
-        }
-
-        if (can_recv(1, &in, &timestamp) < 0)
-        {
-            char error_reason[256] = {0};
-            can_get_error_reason(error_reason, sizeof(error_reason));
-            // printf("%s\n", error_reason);
-        }
-        else
-        {
-            printf("Received CAN frame with ID 0x%X, DLC %u, data: %02X %02X\n",
-                   in.can_id, in.can_dlc, in.data[0], in.data[1]);
-        }
-
-        sleep_ms(100);
-    }
-
     /* Implicitly calls can_close() for all opened interfaces. */
     can_free_interfaces();
     return EXIT_SUCCESS;
